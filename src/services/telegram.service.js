26d98e919c64efbb887c7e4f63bd600b2ba32e64
NallyTHEdude <rifaqatnawaz25@gmail.com>
2026-02-23 05:45:02 +0000

    added a handleConnectCommand for better scalability

--- src/services/telegram.service.js
+++ src/services/telegram.service.js
@@ -1,11 +1,11 @@
 import { axiosInstance } from './lib/axios.js';
 import { logger, ApiError } from '../utils/index.js';
-import axios from 'axios'; 
+import axios from 'axios';
 import { BASE_API_URL } from '../config/index.js';
 
 const botApi = axiosInstance();
 
-function sendMessage(messageObj, messageText, parse_mode=null) {
+function sendMessage(messageObj, messageText, parse_mode = null) {
     if (!messageObj || !messageObj.chat || !messageObj.chat.id) {
         throw new ApiError(400, 'Invalid message object');
     }
@@ -13,42 +13,113 @@ function sendMessage(messageObj, messageText, parse_mode=null) {
     const payload = {
         chat_id: messageObj.chat.id,
         text: messageText,
-    }
+    };
 
-    if(parse_mode) {
+    if (parse_mode) {
         payload.parse_mode = parse_mode;
     }
 
-    logger.info(`chatId: ${messageObj.chat.id}, message: ${messageText}, parse_mode: ${parse_mode}`);
+    logger.info(
+        `chatId: ${messageObj.chat.id}, message: ${messageText}, parse_mode: ${parse_mode}`,
+    );
 
     return botApi.post('sendMessage', payload);
 }
 
-function sendMessageExport(chatId, messageText, parse_mode=null) {
+function sendMessageExport(chatId, messageText, parse_mode = null) {
     if (!chatId) throw new ApiError(400, 'Invalid  chatId');
-    if (!text.trim()) throw new ApiError(400, 'Invalid text length');
+    if (!messageText || !messageText.trim())
+        throw new ApiError(400, 'Invalid text length');
 
     const payload = {
         chat_id: chatId,
         text: messageText,
-    }
-    if(parse_mode) {
+    };
+    if (parse_mode) {
         payload.parse_mode = parse_mode;
     }
 
-    logger.info(`chatId: ${chatId}, message: ${text}, parse_mode: ${parse_mode}`);
+    logger.info(
+        `chatId: ${chatId}, message: ${messageText}, parse_mode: ${parse_mode}`,
+    );
 
     return botApi.post('sendMessage', payload);
 }
 
+const handleConnectCommand = async (messageObj) => {
+    try {
+        // call url to get auth url's
+        const getGoogleUrl = `${BASE_API_URL}/api/v1/auth/google?telegramId=${messageObj.chat.id}`;
+        const githubUrl = `${BASE_API_URL}/api/v1/auth/github?telegramId=${messageObj.chat.id}`;
+        const microsoftUrl = `${BASE_API_URL}/api/v1/auth/microsoft?telegramId=${messageObj.chat.id}`;
+
+        // make parallel requests to get all auth urls
+        // const [googleResponse, githubResponse, microsoftResponse] =
+        // await Promise.all([
+        //     axios.get(getGoogleUrl),
+        //     axios.get(githubUrl),
+        //     axios.get(microsoftUrl),
+        // ]);
+        const googleResponse = await axios.get(getGoogleUrl);
+        const githubResponse = await axios.get(githubUrl);
+        const microsoftResponse = await axios.get(microsoftUrl);
+
+        // extract urls from responses
+        const googleUrl = googleResponse?.data?.data?.url;
+        const githubAuthUrl = githubResponse?.data?.data?.url;
+        const microsoftAuthUrl = microsoftResponse?.data?.data?.url;
+
+        // handle url errors
+        if (!googleUrl) {
+            logger.error('Invalid Google auth response:', googleResponse.data);
+            return sendMessage(
+                messageObj,
+                'Failed to retrieve Google authentication URL. Please try again later.',
+            );
+        }
+        if (!githubAuthUrl) {
+            logger.error('Invalid Github auth response:', githubResponse.data);
+            return sendMessage(
+                messageObj,
+                'Failed to retrieve Github authentication URL. Please try again later.',
+            );
+        }
+        if (!microsoftAuthUrl) {
+            logger.error(
+                'Invalid Microsoft auth response:',
+                microsoftResponse.data,
+            );
+            return sendMessage(
+                messageObj,
+                'Failed to retrieve Microsoft authentication URL. Please try again later.',
+            );
+        }
+
+        // send message with auth urls
+        return sendMessage(
+            messageObj,
+            `Google: <a href="${googleUrl}">Google link</a>\n` +
+                `GitHub: <a href="${githubAuthUrl}">GitHub link</a>\n` +
+                `Microsoft: <a href="${microsoftAuthUrl}">Microsoft link</a>`,
+            'HTML',
+        );
+    } catch (error) {
+        logger.error('Error fetching Google auth URL:', error.message);
+        return sendMessage(
+            messageObj,
+            'An error occurred while connecting to Google. Please try again later.',
+        );
+    }
+};
+
 async function handleMessage(messageObj) {
     const messageText = messageObj.text?.trim();
     if (!messageText) {
-        return sendMessage(messageObj, "I only understand text messages.");
+        return sendMessage(messageObj, 'I only understand text messages.');
     }
-    
+
     if (messageText.charAt(0) === '/') {
-        const command = messageText.substr(1).toLowerCase();
+        const command = messageText.slice(1).toLowerCase();
         switch (command) {
             case 'start':
                 return sendMessage(
@@ -61,30 +132,7 @@ async function handleMessage(messageObj) {
                     'Hello! How can I assist you today?',
                 );
             case 'connect':
-                try {
-                    const getGoogleUrl = `${BASE_API_URL}/api/v1/auth/google?telegramId=${messageObj.chat.id}`;
-                    const googleResponse = await axios.get(getGoogleUrl);
-
-                    if (!googleResponse.data || !googleResponse.data.data || !googleResponse.data.data.url) {
-                        logger.error('Invalid response from Google auth endpoint:', googleResponse.data);
-                        return sendMessage(
-                            messageObj,
-                            'Failed to retrieve Google authentication URL. Please try again later.',
-                        );
-                    }
-
-                    return sendMessage(
-                        messageObj,
-                        `Google: <a href="${googleResponse.data.data.url}">Google link</a>`,
-                        'HTML'
-                    );
-                } catch (error) {
-                    logger.error('Error fetching Google auth URL:', error.message);
-                    return sendMessage(
-                        messageObj,
-                        'An error occurred while connecting to Google. Please try again later.',
-                    );
-                }
+                return await handleConnectCommand(messageObj);
             default:
                 return sendMessage(
                     messageObj,
