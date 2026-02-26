001c3f8a996e84f17bca4f83eb237a073fadbe2a
NallyTHEdude <rifaqatnawaz25@gmail.com>
2026-02-26 09:51:42 +0000

    added github webhooks routes

---
+++ src/routes/providers/githubProvider.routes.js
@@ -0,0 +1,7 @@
+import express, {Router} from 'express';
+import { githubWebhookHandler } from '../../controllers/providers/githubProvider.controller.js';
+const router = Router();
+
+router.post('/github/webhook', express.raw({ type: "*/*" }), githubWebhookHandler);
+
+export default router;
